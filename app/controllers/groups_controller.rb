9680c4b9dcbe7b2b1f8017a85eef3cbc24962e30
xieyubai <xieyubai17@gmail.coml>
2016-12-18 19:29:20 +0800

    check owner permission when access edit/update/destroy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,7 @@
 class GroupsController < ApplicationController
 
   before_action :authenticate_user! , only: [:new]
-  before_action :authenticate_user! , only: [:new, :create]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @groups = Group.all
@@ -18,7 +18,7 @@ class GroupsController < ApplicationController
   def edit
     @group = Group.find(params[:id])
   end
-  
+
   def create
      @group = Group.new(group_params)
      @group.user = current_user
@@ -31,21 +31,27 @@ class GroupsController < ApplicationController
    end
 
     def update
-      @group = Group.find(params[:id])
+     @group = Group.find(params[:id])
 
-      @group.update(group_params)
+     if current_user != @group.user
+       redirect_to root_path, alert: "You have no permission."
+     end
 
-     redirect_to groups_path, notice: "Update Success"
-    end
+     if @group.update(group_params)
+       redirect_to groups_path, notice: "Update Success"
+     else
+       render :edit
+     end
+   end
 
-    def update
+ def edit
    @group = Group.find(params[:id])
-   if @group.update(group_params)
-     redirect_to groups_path, notice: "Update Success"
-   else
-     render :edit
+
+   if current_user != @group.user
+     redirect_to root_path, alert: "You have no permission."
    end
  end
+
 
 
      def destroy
@@ -55,9 +61,20 @@ class GroupsController < ApplicationController
    redirect_to groups_path
  end
 
+ def destroy
+   @group = Group.find(params[:id])
+
+   if current_user != @group.user
+     redirect_to root_path, alert: "You have no permission."
+   end
+
+   @group.destroy
+   redirect_to groups_path, alert: "Group deleted"
+ end
+  end
+
    private
 
   def group_params
      params.require(:group).permit(:title, :description)
    end
-end
